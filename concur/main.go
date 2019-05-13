7301bb8f4aeaef841a34f1928f063ec1cd0036f6
Roshan Fernandes <rfernandes@freewheel.tv>
2019-05-13 11:06:04 -0400

    added error handling

--- concur/main.go
+++ concur/main.go
@@ -2,34 +2,35 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"sync"
 	"sync/atomic"
 )
 
+var theLimit int64 = 2000
+
 func main() {
-	gopher1()
+	gopher2()
 }
 
 func gopher1() {
 	var mux sync.Mutex
 	done := make(chan bool, 1)
+	errCh := make(chan error, 1)
 	workers := make(chan bool, 10)
 	results := make(map[int64]int64)
-	page := int64(1)
+	page := int64(0)
 	var wg sync.WaitGroup
 
 	for {
 		select {
+		case e := <-errCh:
+			fmt.Printf("%v", e)
+			printResults(results)
+			return
 		case <-done:
-			count := 0
 			wg.Wait()
-			for k, r := range results {
-				if k <= 100 {
-					fmt.Println(r)
-					count++
-				}
-			}
-			fmt.Printf("Count %v\n", count)
+			printResults(results)
 			return
 		default:
 			workers <- true
@@ -40,7 +41,10 @@ func gopher1() {
 					wg.Done()
 				}()
 				pg := atomic.AddInt64(&page, 1)
-				res := process(pg)
+				res, err := process(pg, theLimit)
+				if err != nil {
+					errCh <- err
+				}
 				mux.Lock()
 				results[pg] = res
 				mux.Unlock()
@@ -52,17 +56,23 @@ func gopher1() {
 	}
 }
 
+type theResult struct {
+	Page   int64
+	Result int64
+	Err    error
+}
+
 func gopher2() {
 	done := make(chan struct{})
 	defer close(done)
 
 	requests := make(chan int64)
-	results := make(chan int64)
+	results := make(chan *theResult)
 	go func() {
 		defer close(requests)
-		for page:= int64(1); page <= 1000; page++ {
+		for page := int64(1); page <= 100; page++ {
 			select {
-			case requests <-page:
+			case requests <- page:
 			case <-done:
 				return
 			}
@@ -74,12 +84,7 @@ func gopher2() {
 	for i := 0; i < 10; i++ {
 		go func() {
 			for req := range requests {
-				res := process(req)
-				select {
-				case results <- res:
-				case <-done:
-					return
-				}
+				processWrapper(req, theLimit, results, done)
 			}
 			wg.Done()
 		}()
@@ -92,12 +97,39 @@ func gopher2() {
 	count := 0
 	for r := range results {
 		fmt.Println(r)
+		if r.Err != nil {
+			fmt.Println(r.Err)
+			return
+		}
 		count++
 	}
 	fmt.Printf("Count %v\n", count)
-
 }
 
-func process(pg int64) int64{
-	return 2 * pg
+func process(pg int64, limit int64) (int64, error) {
+	result := 2 * pg
+	if result > limit {
+		return result, fmt.Errorf("%v is above limit", result)
+	}
+	return result, nil
+}
+
+func processWrapper(pg int64, limit int64, resultsCh chan<- *theResult, done <-chan struct{}) {
+	result, err := process(pg, limit)
+	select {
+	case resultsCh <- &theResult{Page: pg, Result: result, Err: err}:
+	case <-done:
+		return
+	}
+}
+
+func printResults(results map[int64]int64) {
+	var keys []int64
+	for k := range results {
+		keys = append(keys, k)
+	}
+	sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
+	for _, k := range keys {
+		fmt.Println("Key:", k, "Value:", results[k])
+	}
 }
